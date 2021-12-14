f2555c0861866798a6952d3d06b3fa38f6e2873c
SMMH <smmh72@gmail.com>
2021-12-15 01:26:05 +0330

    added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    implementation("org.jetbrains:annotations:23.0.0")
+    implementation("org.json:json:20211205")
 }
 
 tasks.getByName<Test>("test") {
